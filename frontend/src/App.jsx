3f2369bc7de1be32f23bc4358773cbdf736790df
Reynaldo Silva <reynaldo.silva0101@gmail.com>
2024-08-23 17:02:50 -0400

    testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Hello im testing webhooks.
+        Hello im testing webhooks again!
       </p>
     </>
   )
